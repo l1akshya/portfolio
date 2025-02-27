9849ab9af0654a7825eb6e8036da8ccd23bae1be
Lokesh Hariramani <lakshya1hariramani@gmail.com>
2025-02-27 16:54:35 +0530

    improved

--- app/page.tsx
+++ app/page.tsx
@@ -4,45 +4,52 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import "./home.css";
 
-
 export default function Home() {
+  const [text, setText] = useState("");
+  const fullText = ["Hi I am", "Lokesh"]; // Two separate lines
+  const typingSpeed = 100;
+
+  useEffect(() => {
+    let lineIndex = 0;
+    let charIndex = 0;
+    let currentLine = fullText[lineIndex];
+    let typedLines: string[] = ["", ""]; // Store typed text separately
+
+    const interval = setInterval(() => {
+      if (charIndex < currentLine.length) {
+        typedLines[lineIndex] += currentLine.charAt(charIndex);
+        setText(typedLines.join("<br />")); // Ensures correct line breaks
+        charIndex++;
+      } else if (lineIndex < fullText.length - 1) {
+        // Move to next line
+        lineIndex++;
+        charIndex = 0;
+        currentLine = fullText[lineIndex];
+      } else {
+        clearInterval(interval); // Stop typing once done
+      }
+    }, typingSpeed);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="header">
-            <div className="logo-container">
-              <Image
-                 src="/logo.png"
-                 alt="Logo"
-                 width={62.5}
-                 height={93.5}
-                 className="logo"
-               />
-             </div>
-             <h1 className="name">Hariramani</h1>
+    <div className="page">
+      <div className="center">
+        <div className="ring"></div>
 
+        <div className="intro" dangerouslySetInnerHTML={{ __html: text }}></div>
 
-
-             <div className="center">
-              <div className="ring"></div>
-              <div className="intro">
-              <p className="t1">Hi  I  am</p>
-              <p className="t2">Lokesh</p>
-             </div>
-              <div className="char-container">
-                <Image
-                  src="/char.png"
-                  alt="char"
-                  className="char"
-                  width={250}
-                  height={93.5}
-                />
-             </div>
-            </div>
-
-
-
+        <div className="char-container">
+          <Image
+            src="/char.png"
+            alt="char"
+            className="char"
+            width={250}
+            height={93.5}
+          />
+        </div>
+      </div>
     </div>
-
-
   );
 }
-
